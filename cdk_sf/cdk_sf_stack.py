fb6e5c235b03e1c4c5821d11cbbb35118b6b5cfa
jongsoo.bae <js.bae@heumlabs.io>
2022-02-21 19:28:43 +0900

    initial & bucket`heum-staging-a`

--- cdk_sf/cdk_sf_stack.py
+++ cdk_sf/cdk_sf_stack.py
@@ -1,9 +1,12 @@
+import aws_cdk as cdk
 from aws_cdk import (
     # Duration,
     Stack,
     # aws_sqs as sqs,
+    aws_s3 as s3,
 )
 from constructs import Construct
+
 
 class CdkSfStack(Stack):
 
@@ -11,6 +14,10 @@ class CdkSfStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
+
+        bucket = s3.Bucket(
+            self, "heum-staging-a",
+            bucket_name="heum-staging-a", removal_policy=cdk.RemovalPolicy.DESTROY)
 
         # example resource
         # queue = sqs.Queue(
